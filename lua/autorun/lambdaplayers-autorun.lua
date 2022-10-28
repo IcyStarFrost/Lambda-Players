ada512b6c167fa2af6a4179a31d4eca611a69232
IcyStarFrost <frostlegion10@gmail.com>
2022-10-28 13:20:13 -0500

    first commit

---
+++ lua/autorun/lambdaplayers-autorun.lua
@@ -0,0 +1,38 @@
+
+
+--- Include files in the corresponding folders
+-- Autorun files are seperated in folders unlike the ENT include lua files
+if SERVER then
+
+    local serversidefiles = file.Find( "lambdaplayers/autorun_includes/server/*", "LUA", "nameasc" )
+
+    for k, luafile in ipairs( serversidefiles ) do
+        include( "lambdaplayers/autorun_includes/server/" .. luafile )
+        print( "Lambda Players: Included Server Side Lua File [" .. luafile .. "]" )
+    end
+
+end
+
+print("\n\n")
+
+local sharedfiles = file.Find( "lambdaplayers/autorun_includes/shared/*", "LUA", "nameasc" )
+
+for k, luafile in ipairs( sharedfiles ) do
+    include( "lambdaplayers/autorun_includes/shared/" .. luafile )
+    print( "Lambda Players: Included Shared Lua File [" .. luafile .. "]" )
+end
+
+print("\n\n")
+
+
+local clientsidefiles = file.Find( "lambdaplayers/autorun_includes/client/*", "LUA", "nameasc" )
+
+for k, luafile in ipairs( clientsidefiles ) do
+    if SERVER then
+        AddCSLuaFile( "lambdaplayers/autorun_includes/client/" .. luafile )
+    elseif CLIENT then
+        include( "lambdaplayers/autorun_includes/client/" .. luafile )
+        print( "Lambda Players: Included Client Side Lua File [" .. luafile .. "]" )
+    end
+end
+---
---
+++ lua/entities/npc_lambdaplayer.lua
@@ -0,0 +1,86 @@
+AddCSLuaFile()
+
+ENT.Base = "base_nextbot"
+ENT.Spawnable = true
+ENT.PrintName = "Lambda Player"
+ENT.Author = "StarFrost"
+
+--- Include files based on sv_ sh_ or cl_
+local ENTFiles = file.Find( "lambdaplayers/lambda/*", "LUA", "nameasc" )
+
+for k, luafile in ipairs( ENTFiles ) do
+
+    if string.StartWith( luafile, "sv_" ) then -- Server Side Files
+        include( "lambdaplayers/lambda/" .. luafile )
+        print( "Lambda Players: Included Server Side ENT Lua File [" .. luafile .. "]" )
+    elseif string.StartWith( luafile, "sh_" ) then -- Shared Files
+        if SERVER then
+            AddCSLuaFile( "lambdaplayers/lambda/" .. luafile )
+        end
+        include( "lambdaplayers/lambda/" .. luafile )
+        print( "Lambda Players: Included Shared ENT Lua File [" .. luafile .. "]" )
+    elseif string.StartWith( luafile, "cl_" ) then -- Client Side Files
+        if SERVER then
+            AddCSLuaFile( "lambdaplayers/lambda/" .. luafile )
+        else
+            include( "lambdaplayers/lambda/" .. luafile )
+            print( "Lambda Players: Included Client Side ENT Lua File [" .. luafile .. "]" )
+        end
+    end
+end
+---
+
+-- Localization
+
+    local random = math.random
+
+--
+
+if CLIENT then
+
+    language.Add( "npc_lambdaplayer", "Lambda Player" )
+
+end
+
+
+function ENT:Initialize()
+
+    if SERVER then
+
+        self:SetModel( _LAMBDAPLAYERSDEFAULTMDLS[ random( #_LAMBDAPLAYERSDEFAULTMDLS ) ] )
+
+    elseif CLIENT then
+
+
+
+    end
+
+end
+
+
+function ENT:Think()
+
+end
+
+function ENT:BodyUpdate()
+    if self.IsMoving then
+        self:BodyMoveXY()
+        return
+    end
+    
+    self:FrameAdvance()
+end
+
+
+function ENT:RunBehaviour()
+
+end
+
+
+
+
+list.Set( "NPC", "npc_lambdaplayer", {
+	Name = "Lambda Player",
+	Class = "npc_lambdaplayer",
+	Category = "Lambda Players"
+})
