7455b06cbb86967e03f20008fae3bb33bc241369
ErmaaS-41 <emana202@yandex.ru>
2022-11-08 22:14:09 +0600

    Changes to AR2, SMG, Shotgun, and Crossbow
     - Added secondary fire for AR2;
     - Improved crossbow bolt damage check
     - Made use of soundscript names for sounds instead of full directories

--- lua/lambdaplayers/lambda/weapons/hl2_smg1.lua
+++ lua/lambdaplayers/lambda/weapons/hl2_smg1.lua
@@ -1,6 +1,5 @@
 local CurTime = CurTime
 local random = math.random
-local bullettbl = {}
 
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
 
@@ -14,63 +13,41 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
         attackrange = 1500,
 
         clip = 45,
+        tracername = "Tracer",
+        damage = 4,
+        spread = 0.15,
+        rateoffire = 0.07,
+        muzzleflash = 1,
+        shelleject = "ShellEject",
+        attackanim = ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1,
+        attacksnd = "Weapon_SMG1.Single",
 
-        reloadtime = 1.6,
+        reloadtime = 1.5,
         reloadanim = ACT_HL2MP_GESTURE_RELOAD_SMG1,
         reloadanimationspeed = 1,
-        reloadsounds = { { 0, "weapons/smg1/smg1_reload.wav" } },
+        reloadsounds = { { 0, "Weapon_SMG1.Reload" } },
 
         callback = function( self, wepent, target )
-            if self.l_Clip <= 0 then self:ReloadWeapon() return end
-
             -- Secondary grenade launcher
-            if math.random(75) == 1 and self:GetRangeSquaredTo(target) <= (1000 * 1000) then
-                self.l_WeaponUseCooldown = CurTime() + random(0.55, 0.75)
-
-                wepent:EmitSound( "weapons/ar2/ar2_altfire.wav", 70, 100, 1, CHAN_WEAPON )
-
-                self:RemoveGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_RPG )
-                self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_RPG )
-
-                local vecThrow = ( (target:GetPos() + target:OBBCenter()) - wepent:GetPos() ):Angle()
-    
+            if random(75) == 1 and self:GetRangeSquaredTo(target) <= ( 1000 * 1000 ) then
                 local grenade = ents.Create( "grenade_ar2" )
                 if IsValid( grenade ) then
-                    grenade:SetPos( (self:GetPos() + self:OBBCenter()) + vecThrow:Forward() * 32 + vecThrow:Up() * 32 )
+                    wepent:EmitSound( "Weapon_SMG1.Double" )
+                    
+                    self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_RPG )
+                    self.l_WeaponUseCooldown = CurTime() + random( 0.55, 0.75 )
+
+                    local vecThrow = ( target:WorldSpaceCenter() - self:EyePos() ):Angle()
+                    grenade:SetPos( self:EyePos() + vecThrow:Forward() * 32 + vecThrow:Up() * 32 )
                     grenade:SetAngles( vecThrow )
                     grenade:SetOwner( self )
                     grenade:Spawn()
-                    grenade:SetVelocity( vecThrow:Forward()*1000 )
-                    grenade:SetLocalAngularVelocity( AngleRand(-400, 400) )
+                    grenade:SetVelocity( vecThrow:Forward() * 1000 )
+                    grenade:SetLocalAngularVelocity( AngleRand( -400, 400 ) )
+                    
+                    return true
                 end
-            else
-                self.l_WeaponUseCooldown = CurTime() + 0.065
-
-                wepent:EmitSound( "weapons/smg1/smg1_fire1.wav", 70, random(95,105), 1, CHAN_WEAPON )
-
-                self:RemoveGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1 )
-                self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_SMG1 )
-
-                self:HandleShellEject( "ShellEject" )
-                self:HandleMuzzleFlash( 1 )
-
-                bullettbl.Attacker = self
-                bullettbl.Damage = 5
-                bullettbl.Force = 5
-                bullettbl.HullSize = 5
-                bullettbl.Num = 1
-                bullettbl.TracerName = tracer or "Tracer"
-                bullettbl.Dir = ( target:WorldSpaceCenter() - wepent:GetPos() ):GetNormalized()
-                bullettbl.Src = wepent:GetPos()
-                bullettbl.Spread = Vector( 0.2, 0.2, 0 )
-                bullettbl.IgnoreEntity = self
-
-                self.l_Clip = self.l_Clip - 1
-
-                wepent:FireBullets( bullettbl )
             end
-            
-            return true
         end,
 
         islethal = true,
