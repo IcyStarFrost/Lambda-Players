97309fbc7a2fd614df6af1e713a1958b89cfa096
IcyStarFrost <frostlegion10@gmail.com>
2022-11-10 14:31:57 -0600

    Function move

--- lua/lambdaplayers/lambda/sh_metarecreations.lua
+++ lua/lambdaplayers/lambda/sh_metarecreations.lua
@@ -3,3 +3,28 @@
 function ENT:Nick()
     return self:GetLambdaName()
 end
+
+-- Returns our eye position
+function ENT:EyePos()
+    return self:GetAttachmentPoint( "eyes" ).Pos
+end
+
+-- Returns our eye angles
+function ENT:EyeAngles()
+    return self:GetAttachmentPoint( "eyes" ).Ang
+end
+
+-- Returns the direction we are looking to
+function ENT:GetAimVector()
+    return self:GetAttachmentPoint( "eyes" ).Ang:Forward()
+end
+
+-- Similar to Real Player's :GetEyeTrace()
+function ENT:GetEyeTrace()
+    local attach = self:GetAttachmentPoint( "eyes" )
+    eyetracetable.start = attach.Pos
+    eyetracetable.endpos = attach.Ang:Forward() * 30000
+    eyetracetable.filter = self
+    local result = Trace( eyetracetable )
+    return result
+end
--- lua/lambdaplayers/lambda/sh_util.lua
+++ lua/lambdaplayers/lambda/sh_util.lua
@@ -181,31 +181,6 @@ function ENT:ShouldTreatAsLPlayer( ent )
     if ent:IsNPC() or ent:IsNextBot() then return false end
 end
 
--- Returns our eye position
-function ENT:EyePos()
-    return self:GetAttachmentPoint( "eyes" ).Pos
-end
-
--- Returns our eye angles
-function ENT:EyeAngles()
-    return self:GetAttachmentPoint( "eyes" ).Ang
-end
-
--- Returns the direction we are looking to
-function ENT:GetAimVector()
-    return self:GetAttachmentPoint( "eyes" ).Ang:Forward()
-end
-
--- Similar to Real Player's :GetEyeTrace()
-function ENT:GetEyeTrace()
-    local attach = self:GetAttachmentPoint( "eyes" )
-    eyetracetable.start = attach.Pos
-    eyetracetable.endpos = attach.Ang:Forward() * 30000
-    eyetracetable.filter = self
-    local result = Trace( eyetracetable )
-    return result
-end
-
 
 -- Turns the Lambda Player into a table of its personal data
 -- See function ENT:ApplyLambdaInfo() to use this data with
