b0537b0522e7c1bee1c12939e51332ccc1adf842
ErmaaS-41 <emana202@yandex.ru>
2023-01-31 20:26:59 +0600

    Added some missing default playermodels to the list

--- lua/lambdaplayers/autorun_includes/server/globals.lua
+++ lua/lambdaplayers/autorun_includes/server/globals.lua
@@ -28,11 +28,16 @@ _LAMBDAPLAYERS_DefaultPlayermodels = {
     "models/player/odessa.mdl",
     "models/player/phoenix.mdl",
     "models/player/police.mdl",
+    "models/player/police_fem.mdl",
     "models/player/riot.mdl",
     "models/player/skeleton.mdl",
     "models/player/soldier_stripped.mdl",
     "models/player/swat.mdl",
     "models/player/urban.mdl",
+    "models/player/hostage/hostage_01.mdl",
+    "models/player/hostage/hostage_02.mdl",
+    "models/player/hostage/hostage_03.mdl",
+    "models/player/hostage/hostage_04.mdl",
     "models/player/Group01/female_01.mdl",
     "models/player/Group01/female_02.mdl",
     "models/player/Group01/female_03.mdl",
@@ -88,6 +93,7 @@ _LAMBDAPLAYERS_DefaultPlayermodels = {
     "models/player/mossman_arctic.mdl",
     "models/player/magnusson.mdl",
     "models/player/monk.mdl",
+    "models/player/zombie_classic.mdl",
     "models/player/zombie_fast.mdl"
 }
 
