aee3f0cef2fe2807d862cd749a21fb1030980c18
IcyStarFrost <frostlegion10@gmail.com>
2023-02-14 18:22:40 -0600

    Added Playermodel Blocking Panel

---
+++ lua/lambdaplayers/autorun_includes/server/playermodels.lua
@@ -0,0 +1,139 @@
+local table_remove = table.remove
+local table_Copy = table.Copy
+local table_ClearKeys = table.ClearKeys
+local ipairs = ipairs
+local PrecacheModel = util.PrecacheModel
+
+
+function LambdaUpdatePlayerModels()
+    local models = table_ClearKeys( table_Copy( player_manager.AllValidModels() ) )
+    _LAMBDAPLAYERS_AllPlayermodels = models
+    _LAMBDAPLAYERS_DefaultPlayermodels = {
+        "models/player/alyx.mdl",
+        "models/player/arctic.mdl",
+        "models/player/barney.mdl",
+        "models/player/breen.mdl",
+        "models/player/charple.mdl",
+        "models/player/combine_soldier.mdl",
+        "models/player/combine_soldier_prisonguard.mdl",
+        "models/player/combine_super_soldier.mdl",
+        "models/player/corpse1.mdl",
+        "models/player/dod_american.mdl",
+        "models/player/dod_german.mdl",
+        "models/player/eli.mdl",
+        "models/player/gasmask.mdl",
+        "models/player/gman_high.mdl",
+        "models/player/guerilla.mdl",
+        "models/player/kleiner.mdl",
+        "models/player/leet.mdl",
+        "models/player/odessa.mdl",
+        "models/player/phoenix.mdl",
+        "models/player/police.mdl",
+        "models/player/police_fem.mdl",
+        "models/player/riot.mdl",
+        "models/player/skeleton.mdl",
+        "models/player/soldier_stripped.mdl",
+        "models/player/swat.mdl",
+        "models/player/urban.mdl",
+        "models/player/hostage/hostage_01.mdl",
+        "models/player/hostage/hostage_02.mdl",
+        "models/player/hostage/hostage_03.mdl",
+        "models/player/hostage/hostage_04.mdl",
+        "models/player/Group01/female_01.mdl",
+        "models/player/Group01/female_02.mdl",
+        "models/player/Group01/female_03.mdl",
+        "models/player/Group01/female_04.mdl",
+        "models/player/Group01/female_05.mdl",
+        "models/player/Group01/female_06.mdl",
+        "models/player/Group01/male_01.mdl",
+        "models/player/Group01/male_02.mdl",
+        "models/player/Group01/male_03.mdl",
+        "models/player/Group01/male_04.mdl",
+        "models/player/Group01/male_05.mdl",
+        "models/player/Group01/male_06.mdl",
+        "models/player/Group01/male_07.mdl",
+        "models/player/Group01/male_08.mdl",
+        "models/player/Group01/male_09.mdl",
+        "models/player/Group02/male_02.mdl",
+        "models/player/Group02/male_04.mdl",
+        "models/player/Group02/male_06.mdl",
+        "models/player/Group02/male_08.mdl",
+        "models/player/Group03/female_01.mdl",
+        "models/player/Group03/female_02.mdl",
+        "models/player/Group03/female_03.mdl",
+        "models/player/Group03/female_04.mdl",
+        "models/player/Group03/female_05.mdl",
+        "models/player/Group03/female_06.mdl",
+        "models/player/Group03/male_01.mdl",
+        "models/player/Group03/male_02.mdl",
+        "models/player/Group03/male_03.mdl",
+        "models/player/Group03/male_04.mdl",
+        "models/player/Group03/male_05.mdl",
+        "models/player/Group03/male_06.mdl",
+        "models/player/Group03/male_07.mdl",
+        "models/player/Group03/male_08.mdl",
+        "models/player/Group03/male_09.mdl",
+        "models/player/Group03m/female_01.mdl",
+        "models/player/Group03m/female_02.mdl",
+        "models/player/Group03m/female_03.mdl",
+        "models/player/Group03m/female_04.mdl",
+        "models/player/Group03m/female_05.mdl",
+        "models/player/Group03m/female_06.mdl",
+        "models/player/Group03m/male_01.mdl",
+        "models/player/Group03m/male_02.mdl",
+        "models/player/Group03m/male_03.mdl",
+        "models/player/Group03m/male_04.mdl",
+        "models/player/Group03m/male_05.mdl",
+        "models/player/Group03m/male_06.mdl",
+        "models/player/Group03m/male_07.mdl",
+        "models/player/Group03m/male_08.mdl",
+        "models/player/Group03m/male_09.mdl",
+        "models/player/zombie_soldier.mdl",
+        "models/player/p2_chell.mdl",
+        "models/player/mossman.mdl",
+        "models/player/mossman_arctic.mdl",
+        "models/player/magnusson.mdl",
+        "models/player/monk.mdl",
+        "models/player/zombie_classic.mdl",
+        "models/player/zombie_fast.mdl"
+    }
+
+    _LAMBDAPLAYERS_AddonPlayermodels = table_Copy( models )
+    for _, v in ipairs( _LAMBDAPLAYERS_DefaultPlayermodels ) do
+        PrecacheModel( v ) -- Precache every default model
+        for k, j in ipairs( _LAMBDAPLAYERS_AddonPlayermodels ) do
+            if j == v then table_remove( _LAMBDAPLAYERS_AddonPlayermodels, k ) end
+        end
+    end
+
+    local blockdata = LAMBDAFS:ReadFile( "lambdaplayers/pmblockdata.json", "json" )
+
+    if blockdata and istable( blockdata ) then
+        for k, mdl in ipairs( blockdata ) do 
+            table.RemoveByValue( _LAMBDAPLAYERS_DefaultPlayermodels, mdl )
+            table.RemoveByValue( _LAMBDAPLAYERS_AddonPlayermodels, mdl )
+            table.RemoveByValue( _LAMBDAPLAYERS_AllPlayermodels, mdl )
+        end
+    end
+
+    local defaultcount = table.Count( _LAMBDAPLAYERS_DefaultPlayermodels )
+    local allcount = table.Count( _LAMBDAPLAYERS_AllPlayermodels )
+    local addoncount = table.Count( _LAMBDAPLAYERS_AddonPlayermodels )
+
+    if defaultcount == 0 then 
+        _LAMBDAPLAYERS_DefaultPlayermodels[ 1 ] = "models/player/kleiner.mdl"
+        print( "Lambda Players Warning: All Default Playermodels were blocked! Adding Kleiner model to default models to prevent issues!")
+    end
+
+    if allcount == 0 then 
+        _LAMBDAPLAYERS_DefaultPlayermodels[ 1 ] = "models/player/kleiner.mdl"
+    end
+
+    if addoncount == 0 then 
+        _LAMBDAPLAYERS_AddonPlayermodels[ 1 ] = "models/player/kleiner.mdl"
+        print( "Lambda Players Warning: All Playermodels were blocked! Adding Kleiner model to addon models to prevent issues!")
+    end
+
+end
+
+LambdaUpdatePlayerModels()
---
+++ lua/lambdaplayers/lambda/panels/playermodelblock.lua
@@ -0,0 +1,69 @@
+local function OpenPlayermodelBlockPanel( ply )
+    if !ply:IsSuperAdmin() then notification.AddLegacy( "You must be a Super Admin in order to use this!", 1, 4) surface.PlaySound( "buttons/button10.wav" ) return end
+
+    local frame = LAMBDAPANELS:CreateFrame( "Playermodel Blocking", 600, 500 )
+    LAMBDAPANELS:CreateLabel( "Click on playermodels to the left to block them. Right click a row on the right to unblock a model", frame, TOP )
+    local leftpnl = vgui.Create( "DPanel", frame )
+    local rightpnl = vgui.Create( "DPanel", frame )
+    local playermodelscroll = LAMBDAPANELS:CreateScrollPanel( leftpnl, false, FILL )
+
+    leftpnl:SetSize( 290, 1 )
+    leftpnl:Dock( LEFT )
+    rightpnl:SetSize( 300, 1 )
+    rightpnl:DockMargin( 10, 0, 0, 0 )
+    rightpnl:Dock( LEFT )
+
+    
+    LAMBDAPANELS:CreateLabel( "Playermodels", leftpnl, TOP )
+
+
+    local mdllayout = vgui.Create( "DIconLayout", playermodelscroll )
+    mdllayout:Dock( FILL )
+    mdllayout:SetSpaceX( 5 )
+    mdllayout:SetSpaceY( 5 )
+
+    local blockedlist = vgui.Create( "DListView", rightpnl )
+    blockedlist:Dock( FILL )
+    blockedlist:AddColumn( "Blocked Playermodels", 1 )
+
+
+    function blockedlist:OnRowRightClick( id, line )
+        local icon = mdllayout:Add( "SpawnIcon" )
+        icon:SetModel( line:GetColumnText( 1 ) )
+        self:RemoveLine( id )
+    end
+    
+
+    for k, mdl in pairs( player_manager.AllValidModels() ) do
+        local icon = mdllayout:Add( "SpawnIcon" )
+        icon:SetModel( mdl )
+
+        function icon:DoClick()
+            blockedlist:AddLine( mdl )
+            self:Remove()
+        end
+    end
+
+    function frame:OnClose()
+        local mdls = {}
+        for k, line in pairs( blockedlist:GetLines() ) do mdls[ #mdls + 1 ] = line:GetColumnText( 1 ) end
+        LAMBDAFS:WriteFile( "lambdaplayers/pmblockdata.json", mdls, "json", false ) 
+    end
+
+    LAMBDAPANELS:RequestDataFromServer( "lambdaplayers/pmblockdata.json", "json", function( data )
+        if !data then return end
+
+        for k, mdl in pairs( data ) do 
+            blockedlist:AddLine( mdl )
+
+            for _, icon in pairs( mdllayout:GetChildren() ) do
+                if icon:GetModelName() == mdl then icon:Remove() break end
+            end
+
+        end
+
+    end )
+
+
+end
+RegisterLambdaPanel( "PlayermodelBlock", "Opens a panel that allows you to prevent Lambdas from using certain playermodels", OpenPlayermodelBlockPanel )
