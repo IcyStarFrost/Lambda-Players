ae9628a3fef7910a8ab8f43c8999cee994f72416
CombineSlayer24 <luvangel157@aol.com>
2023-02-16 06:15:04 -0700

    Adds more TF2 Sentry Buster stuff to PAIG from Zeta Players

--- lua/lambdaplayers/lambda/weapons/misc_paig.lua
+++ lua/lambdaplayers/lambda/weapons/misc_paig.lua
@@ -9,6 +9,8 @@ local CreateSound = CreateSound
 local Rand = math.Rand
 local ipairs = ipairs
 
+game.AddParticles("particles/bigboom.pcf")
+
 local busterMode = CreateLambdaConvar( "lambdaplayers_weapons_paigsentrybuster", 0, true, false, true, "If Lambda that spawn with the PAIG have the ability to act like the Sentry Buster from TF2.", 0, 1, { type = "Bool", name = "PAIG - Enable Sentry Buster Mode", category = "Weapon Utilities" } )
 
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
@@ -73,7 +75,7 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
             end
 
             for _, v in ipairs( GetLambdaPlayers() ) do
-                if v != self and LambdaIsValid( v ) and v:IsInRange( self, 400 ) and v:CanTarget( self ) and ( wepent.SentryBusterMode or v:CanSee( self ) ) then
+                if v != self and LambdaIsValid( v ) and v:IsInRange( self, 400 ) or ( wepent.SentryBusterMode and v:IsInRange( self, 750 ) ) and v:CanTarget( self ) and ( wepent.SentryBusterMode or v:CanSee( self ) ) then
                     v:SimpleTimer( Rand( 0.0, 0.25 ), function() v:RetreatFrom( self, 1.0 + detonateTime ) end)
                 end
             end
@@ -97,7 +99,11 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
                 effectData:SetOrigin( blowPos )
                 Effect( "Explosion", effectData, true, true )
 
-                BlastDamage( wepent, self, blowPos, 400, 1000 )
+                if !wepent.SentryBusterMode then
+                    BlastDamage( wepent, self, blowPos, 400, 1000 )
+                elseif wepent.SentryBusterMode then
+                    BlastDamage( wepent, self, blowPos, 750, 600 ) // Sentrybuster does 600 damage, and has a larger radius
+                end
 
                 local selfDmg = DamageInfo()
                 selfDmg:SetDamage( 1000 )
@@ -107,6 +113,19 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
                 self:TakeDamageInfo( selfDmg )
 
                 wepent:EmitSound( detonateSnd, 90 )
+
+                if wepent.SentryBusterMode then
+                    util.ScreenShake(wepent:GetPos(),25,5,3,1000)
+                    
+                    ParticleEffect("fluidSmokeExpl_ring_mvm", wepent:GetPos(), wepent:GetAngles())
+                    ParticleEffect("explosionTrail_seeds_mvm", wepent:GetPos(), wepent:GetAngles())
+    
+                    for _, v in ipairs(ents.FindInSphere(wepent:GetPos(), 1000)) do
+                        if IsValid(v) and v:IsPlayer() then
+                            v:ScreenFade(SCREENFADE.IN, Color(255, 255, 255, 255), 1.0, 0.1)
+                        end
+                    end
+                end  
             end)
 
             return true
