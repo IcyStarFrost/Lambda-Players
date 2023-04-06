c5016caad1088f6f7995012e6ef6e8dac9eede1a
unknown <12295605+JackTheRedCreeper@users.noreply.github.com>
2023-04-07 00:54:03 +0200

    New gravity gun

--- lua/lambdaplayers/lambda/weapons/hl2_gravitygun.lua
+++ lua/lambdaplayers/lambda/weapons/hl2_gravitygun.lua
@@ -17,6 +17,98 @@ local blastTarget = false
 local color = Color(255,255,255,255)
 local attachtab = { "fork1m", "fork1t", "fork2m", "fork2t", "fork3m", "fork3t" }
 
+local LerpVector = LerpVector
+local Angle = Angle
+local random = math.random
+local min = math.min
+local max = math.max
+local lambda = nil
+local wepDmgScale = GetConVar( "lambdaplayers_combat_weapondmgmultiplier" )
+
+local propGrabbed = false 
+local canShoot = false
+
+local pickables = {
+    [ "prop_physics" ] = true,
+    --[ "gb_bomb_sc100" ] = true,
+    --[ "grenade_helicopter" ] = true,
+    --[ "sent_tnt" ] = true,
+}
+local spawnableProps = {
+	
+    "models/props_c17/oildrum001_explosive.mdl",
+	"models/props_c17/oildrum001.mdl",
+	"models/props_c17/FurnitureRadiator001a.mdl",
+	"models/props_c17/FurnitureChair001a.mdl",
+	"models/props_c17/canister01a.mdl",
+	"models/props_c17/canister_propane01a.mdl",
+	"models/Combine_Helicopter/helicopter_bomb01.mdl",
+	"models/props_interiors/Furniture_Couch02a.mdl",
+	"models/props_junk/sawblade001a.mdl",
+	"models/props_junk/gascan001a.mdl",
+	"models/props_junk/CinderBlock01a.mdl",
+	"models/props_junk/harpoon002a.mdl",
+	"models/props_junk/PopCan01a.mdl",
+	"models/props_junk/metal_paintcan001a.mdl",
+	"models/props_junk/propane_tank001a.mdl",
+	"models/props_junk/PropaneCanister001a.mdl",
+	"models/props_junk/TrafficCone001a.mdl",
+	"models/props_junk/wood_crate001a.mdl",
+	"models/props_junk/wood_crate002a.mdl",
+	"models/props_junk/wood_pallet001a.mdl",
+	"models/props_lab/filecabinet02.mdl",
+	"models/props_trainstation/BenchOutdoor01a.mdl",
+	"models/props_trainstation/TrackSign02.mdl",
+	"models/props_trainstation/trainstation_post001.mdl",
+	"models/props_trainstation/trashcan_indoor001b.mdl",
+	"models/props_vehicles/tire001a_tractor.mdl",
+	"models/props_vehicles/tire001c_car.mdl",
+	"models/props_wasteland/barricade001a.mdl",
+	"models/props_wasteland/barricade002a.mdl",
+	"models/props_wasteland/kitchen_counter001b.mdl",
+	"models/props_junk/watermelon01.mdl",
+	"models/props_junk/terracotta01.mdl",
+	"models/props_lab/cactus.mdl",
+	"models/props_combine/breenbust.mdl",
+	"models/props_interiors/Furniture_chair01a.mdl",
+	"models/props_interiors/Furniture_Desk01a.mdl",
+	"models/props_junk/GlassBottle01a.mdl",
+	"models/props_junk/garbage_coffeemug001a.mdl",
+	"models/props_junk/garbage_metalcan002a.mdl",
+	"models/props_junk/bicycle01a.mdl",
+	"models/props_wasteland/prison_toilet01.mdl",
+}
+
+
+local function SpawnProp(self)
+	if !self:IsUnderLimit( "Prop" ) then self:UndoLastSpawnedEnt() end
+	local mdl = spawnableProps[ random( #spawnableProps ) ]
+
+	if !mdl then return end
+
+	self:EmitSound( "ui/buttonclickrelease.wav", 60 )
+
+	local prop = ents.Create( "prop_physics" )
+	prop:SetPos( self:GetPos() + self:GetForward()*25 )
+	prop:SetAngles( Angle( 0, self:GetAngles()[ 2 ], 0 ) )
+	prop:SetModel( mdl )
+	prop.selfOwner = self
+	prop.IsselfSpawned = true
+	prop:Spawn()
+	DoPropSpawnedEffect( prop ) -- Make the prop do the spawn effect
+
+	local mins = prop:GetModelBounds()
+	local proppos = prop:GetPos()
+	proppos[ 3 ] = proppos[ 3 ] - mins[ 3 ]
+	prop:SetPos( proppos )
+
+	self:DebugPrint( "spawned a prop while fighting ", prop )
+
+	self:ContributeEntToLimit( prop, "Prop" )
+	table.insert( self.l_SpawnedEntities, 1, prop )
+	return prop
+end
+
 table.Merge( _LAMBDAPLAYERSWEAPONS, {
 
     gravgun = {
@@ -26,8 +118,12 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
         killicon = "weapon_physcannon", -- No idea if this is needed but why not
         bonemerge = true,
         holdtype = "physgun",
-
+        keepdistance = 500,
+        attackrange = 600,
+		
         OnDraw = function( lambda, wepent )
+			
+			lambda = lambda
             if IsValid( wepent ) then
                 for i = 1, #attachtab do
                     local atID = wepent:LookupAttachment( attachtab[i] )
@@ -47,104 +143,145 @@ table.Merge( _LAMBDAPLAYERSWEAPONS, {
                 render.SetMaterial( ggunCoreSprite )
                 render.DrawSprite( wepent:GetAttachment( wepent:LookupAttachment( "core" ) ).Pos, 13, 13, Color(255,255,255,255) )
                 ]]
-            end
+			end
+		end,
+		
+		
+        OnDeploy = function( lambda, wepent )
+			propGrabbed = false
+			canShoot = false
+			lambda.attachedProp = nil
+			
+			lambda:Thread( function() -- Look for props and pick em up, or spawn some if there are none nearby.
+
+				while true do
+					
+					
+					-- Check that we don't have a prop (it may have been destroyed)
+					-- Important to keep this up here so we can benefeit from coroutine.wait()
+					if (!IsValid(lambda.attachedProp)) then
+						propGrabbed = false
+					end
+					
+					
+					-- We don't have a prop
+					if propGrabbed == false then
+						print("we don't have a prop")
+						
+						-- Find a prop around us to pick it up
+						local find = lambda:FindInSphere( lambda:GetPos(), 150, function( ent ) if !ent:IsNPC() and ent:GetClass() == "prop_physics" and !ent:IsPlayer() and !ent:IsNextBot() and lambda:CanSee( ent ) and IsValid( ent:GetPhysicsObject() ) and lambda:HasPermissionToEdit( ent ) and ent:GetPhysicsObject():IsMoveable() then return true end end )
+						local prop = find[ random( #find ) ]
+						
+						-- We couldn't find a prop, spawn one
+						if prop == nil then prop = SpawnProp(lambda) end 
+						
+						-- Still no prop, abort
+						if prop == nil then continue end
+						
+						-- Ignore prop if it doesn't have a physics object
+						if (prop:GetPhysicsObject() == nil) then continue end
+						-- If the prop is frozen, ignore it
+						if (prop:GetPhysicsObject():IsMotionEnabled() == false) then continue end
+						-- If the prop is not in the normal collision group (another bot has it, for example), ignore it
+						if (prop:GetCollisionGroup() != 0) then prop = nil end 
+						
+						
+						
+						if IsValid(prop) and prop != nil then
+							-- Short delay between spawning the prop and picking it up
+							lambda:LookTo( prop, 0.5 )
+							
+							-- Check if prop is still valid
+							if IsValid(prop) and prop != nil then 
+								-- Set this prop as ours
+								lambda.attachedProp = prop
+								-- Unfreeze prop just in case
+								lambda.attachedProp:GetPhysicsObject():EnableMotion( true )
+								-- Set the prop's collision group so it doesn't collide with the world and causes constant bot deaths
+								lambda.attachedProp:SetCollisionGroup(COLLISION_GROUP_DEBRIS)
+								-- Set ourselves as having a prop
+								propGrabbed = true
+								print("we have a prop now")
+								print(propGrabbed)
+								print(lambda.attachedProp)
+								-- Shorten combat distances as we're ready to pummel others!
+								keepdistance = 100
+								attackrange = 500
+								canShoot = true
+							end
+						end
+					end
+					
+					
+					-- while loop delay, don't remove this if you don't like freezing
+					coroutine.wait(1)
+				end
+			end, "PhysgunThread" )
+			
+			-- Hook that keeps the grabbed prop attached to the lambda
+			lambda:Hook( "Think", "physgunthink", function()
+				-- Check validity of prop while we have it
+				if IsValid( lambda.attachedProp ) and propGrabbed == true then
+					-- Set prop's position
+					lambda.attachedProp:GetPhysicsObject():SetPos(lambda:GetPos() + lambda:GetForward() * 75 + lambda:GetUp() * 36)
+				end
+			end )
         end,
 
-        OnThink = function( lambda, wepent, dead )
-            if !dead then
-                -- Pretty much will punt any prop they get close to
-                local find = lambda:FindInSphere( lambda:GetPos(), 150, function( ent ) if !ent:IsNPC() and ent:GetClass()=="prop_physics" and !ent:IsPlayer() and !ent:IsNextBot() and lambda:CanSee( ent ) and IsValid( ent:GetPhysicsObject() ) and lambda:HasPermissionToEdit( ent ) and ent:GetPhysicsObject():IsMoveable() then return true end end )
-                local prop = find[ random( #find ) ]
-
-                lambda:LookTo( prop, 3 )
-
-                lambda:SimpleWeaponTimer( 1, function() -- To let the Lambda aim properly
-                    if !IsValid( prop ) then return end
-                    lambda:UseWeapon( prop )
-                end)
-            end
-
-            return 1.0 -- "Attack" prop twice then do another check (1)
+        OnHolster = function( lambda, wepent )
+			-- Reset prop's collision groups
+			if IsValid(lambda.attachedProp) and (lambda.attachedProp != nil) then lambda.attachedProp:SetCollisionGroup(COLLISION_GROUP_NONE) end
+			lambda.attachedProp = nil
+            lambda:KillThread( "PhysgunThread" )
+			propGrabbed = false
+			canShoot = false
+			keepdistance = 500
+			attackrange = 600
         end,
-
+		
+		
         OnAttack = function( self, wepent, target )
-            self.l_WeaponUseCooldown = CurTime() + 0.4
+		
+			-- Weapon cooldown
+			self.l_WeaponUseCooldown = CurTime() + 1
+			-- Look at our next victim >:)
+			self:LookTo( target, 1)
+		
+		
+			if canShoot == true then
+				print("we're shooting a prop")
+				
+				-- Check for prop integrity and if we're carrying one
+				if IsValid( self.attachedProp ) and self.attachedProp != nil and propGrabbed == true then
+				
+					wepent:EmitSound( "weapons/physcannon/superphys_launch"..random( 1, 4 )..".wav", 70, random( 110, 120 ) )
 
-            local phys = target:GetPhysicsObjectNum(0)
+					local mainPhys = self.attachedProp:GetPhysicsObject()
+					local trace = self:Trace( self.attachedProp:WorldSpaceCenter() )
+					local randt1 = self:Trace( self.attachedProp:WorldSpaceCenter() + VectorRand(-5, 15) )
 
-            if IsValid( phys ) then
-                if !self:IsInRange( target, 175 ) then wepent:EmitSound( "weapons/physcannon/physcannon_dryfire.wav", 70 ) return end
-                wepent:EmitSound( "weapons/physcannon/superphys_launch"..random( 1, 4 )..".wav", 70, random( 110, 120 ) )
+					mainPhys:ApplyForceCenter( self:GetAimVector() * (100000*wepDmgScale:GetFloat()) + self:GetUp()*2500)
 
-                --[[
-                    for i = 0, target:GetPhysicsObjectCount() - 1 do
-                    local subphys = target:GetPhysicsObjectNum(i)
-                    totalMass = totalMass + subphys:GetMass()
-                    end
+					local core = wepent:GetAttachment( wepent:LookupAttachment( "core" ) )
 
-                    local actualMass = math_min(totalMass, 250)
-                    local mainPhys = target:GetPhysicsObject()
 
-                    for i = 0, target:GetPhysicsObjectCount() - 1 do
-                        local subphys = target:GetPhysicsObjectNum(i)
-                        local ratio = phys:GetMass() / totalMass
-                        if subphys == mainPhys then
-                            ratio = ratio + 0.5
-                            ratio = math_min(ratio, 1.0)
-                        else
-                            ratio = ratio * 0.5
-                        end
-
-                        subphys:ApplyForceCenter( self:GetAimVector() * 15000 * ratio)
-                        subphys:ApplyForceOffset( self:GetAimVector() * actualMass * 600 * ratio, trace.HitPos)
-                    end
-                ]]-- If we ever want to punt ragdolls
-
-                local mainPhys = target:GetPhysicsObject()
-                local trace = self:Trace( target:WorldSpaceCenter() )
-                local randt1 = self:Trace( target:WorldSpaceCenter() + VectorRand(-5, 15) )
-
-                mainPhys:ApplyForceCenter( self:GetAimVector() * 15000)
-                mainPhys:ApplyForceOffset( self:GetAimVector() * math_min( mainPhys:GetMass(), 250) * 600, trace.HitPos)
-
-                local core = wepent:GetAttachment( wepent:LookupAttachment( "core" ) )
-
-                -- See lua/effects/gravitygunbeam and gravitygunblast
-                -- Simulate the punt beam blast
-                local effectBeam = EffectData()
-                    effectBeam:SetStart( core.Pos )
-                    effectBeam:SetOrigin( trace.HitPos ) 
-                    effectBeam:SetEntity( wepent )
-                util_Effect( "gravitygunbeam", effectBeam, true, true) -- Beam 1 to center of target
-                local effectBlast = EffectData()
-                    effectBlast:SetStart( core.Pos )
-                    effectBlast:SetOrigin( core.Pos )
-                    effectBlast:SetEntity( wepent )
-                util_Effect( "gravitygunblast", effectBlast, true, true) -- Blast effect from Gravity gun
-
-                -- Simulate the spark that emits from punted object
-                local effectSpark = EffectData()
-                    effectSpark:SetOrigin( trace.HitPos )
-                    effectSpark:SetNormal( trace.HitNormal )
-                    effectSpark:SetScale(2)
-                util_Effect( "MetalSpark", effectSpark, true, true) -- 
-                    effectSpark:SetOrigin( trace.HitPos )
-                    effectSpark:SetNormal( trace.HitNormal )
-                    effectSpark:SetScale(2)
-                    effectSpark:SetMagnitude(3)
-                    effectSpark:SetRadius(4)
-                util_Effect( "Sparks", effectSpark, true, true)
-            else
-                wepent:EmitSound( "weapons/physcannon/physcannon_dryfire.wav", 70 )
-            end
-
-            self:RemoveGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_PHYSGUN )
-            self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_PHYSGUN )
-            
-            return true
+					self:RemoveGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_PHYSGUN )
+					self:AddGesture( ACT_HL2MP_GESTURE_RANGE_ATTACK_PHYSGUN )
+					-- Reset prop weapon group
+					if IsValid(self.attachedProp) and (self.attachedProp != nil) then self.attachedProp:SetCollisionGroup(COLLISION_GROUP_NONE) end
+					-- Unset the prop as ours
+					self.attachedProp = nil
+					-- We are no longer carrying a prop
+					propGrabbed = false
+					print("prop was shot")
+				else
+					wepent:EmitSound( "weapons/physcannon/physcannon_dryfire.wav", 70 )
+				end
+				return true
+			end
         end,
 
-        islethal = false
+        islethal = true
 
     }
 
