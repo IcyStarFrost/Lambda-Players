4a764928095fcc253102379113e48f76898a8f03
IcyStarFrost <frostlegion10@gmail.com>
2024-06-09 22:56:21 -0500

    Comments

--- lua/lambdaplayers/autorun_includes/shared/g_fileshare.lua
+++ lua/lambdaplayers/autorun_includes/shared/g_fileshare.lua
@@ -1,5 +1,9 @@
+-- TODO: Make this work with sprays
+
+-- Purpose of this system here is to network files such as voice lines and pfps so other clients can hear and see them without the need of addons
 if SERVER then
 
+    -- Function to split files into chunks
     local function DataSplit( data )
         local index = 1
         local result = {}
@@ -23,13 +27,16 @@ if SERVER then
     net.Receive( "lambdaplayers_sendfile", function( len, ply )
         local filepath = net.ReadString()
         local bytes = 0
+
+        -- Read the file in binary mode. For some reason file.Read() is restricted(?) from reading the sounds folder. I presume it's the same for materials
         local file_ = file.Open( filepath, "rb", "GAME" )
 
         data = file_:Read()
 
         file_:Close()
 
-        if !data then print("FAIL", filepath, data ) return end
+        -- Abort
+        if !data then return end
 
         -- Do not trust the client
         if string.StartWith( filepath, "sound/lambdaplayers/" ) or string.StartWith( filepath, "materials/lambdaplayers/" ) then
@@ -38,9 +45,11 @@ if SERVER then
 
             LambdaCreateThread( function()
                 
+                -- Compress the data to speed up file transfer over the network
                 local compressed = util.Compress( data )
                 local chunks = DataSplit( compressed )
 
+                -- Send each chunk to the client that sent the net message
                 for k, block in ipairs( chunks ) do
 
                     net.Start( "lambdaplayers_sendfile" )
@@ -64,16 +73,18 @@ if SERVER then
 elseif CLIENT then
     file.CreateDir( "lambdaplayers/fileshare" )
 
-    local requestedfiles = {}
-    local datachunks = {}
+    local requestedfiles = {} -- Table holding filepaths that are requested with a callback
+    local datachunks = {} -- Table holding in progress data chunks
 
+    -- Clear the fileshare folder on load to save storage
     for k, v in ipairs( file.Find( "lambdaplayers/fileshare", "DATA" ) ) do
         file.Delete( "lambdaplayers/fileshare/" .. v, "DATA" )
     end
 
+    -- Function to request files RESTRICTED to sound and material folder
     function LambdaRequestFile( filepath, callback )
         if requestedfiles[ filepath ] then return end
-        if file.Exists( "lambdaplayers/fileshare/" .. string.Replace( filepath, "/", "" ), "DATA" ) then
+        if file.Exists( "lambdaplayers/fileshare/" .. string.Replace( filepath, "/", "" ), "DATA" ) then -- Already exists.
             return "lambdaplayers/fileshare/" .. string.Replace( filepath, "/", "" )
         end
 
@@ -91,11 +102,12 @@ elseif CLIENT then
         local isdone = net.ReadBool()
         local filepath = net.ReadString()
 
-
+        -- Build the chunks together
         local holder = datachunks[ filepath ] or ""
         holder = holder .. data
         datachunks[ filepath ] = holder
 
+        -- Decompress, write to a file, and return the file path to the callback
         if isdone then
             local uncompressed = util.Decompress( datachunks[ filepath ] )
             datachunks[ filepath ] = nil 
